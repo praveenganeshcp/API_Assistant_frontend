9f14c6e57e166bba2db4aff677a09801df495f37
praveen kumar <praveen@textmercato.com>
2021-10-04 15:20:41 +0530

    app shell added

--- projects/cpbase-client/src/app/app.module.ts
+++ projects/cpbase-client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppShellComponent } from './components/app-shell/app-shell.component';
 import { LandingComponent } from './components/landing/landing.component';
+import { SharedModule } from './modules/shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { LandingComponent } from './components/landing/landing.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- projects/cpbase-client/src/app/modules/database/database.module.ts
+++ projects/cpbase-client/src/app/modules/database/database.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DatabaseRoutingModule } from './database-routing.module';
-import { ClientComponent } from './pages/client/client.component';
 import { DbClientComponent } from './pages/db-client/db-client.component';
 
 
 @NgModule({
-  declarations: [ClientComponent, DbClientComponent],
+  declarations: [DbClientComponent],
   imports: [
     CommonModule,
     DatabaseRoutingModule
