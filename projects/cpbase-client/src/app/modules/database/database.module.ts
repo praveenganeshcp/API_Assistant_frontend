0d94e871ee11c9a0ae9c43ba8fe55e8461dedd95
praveen kumar <praveen@textmercato.com>
2021-10-04 11:33:15 +0530

    modules created

---
+++ projects/cpbase-client/src/app/modules/database/database.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DatabaseRoutingModule } from './database-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DatabaseRoutingModule
+  ]
+})
+export class DatabaseModule { }
