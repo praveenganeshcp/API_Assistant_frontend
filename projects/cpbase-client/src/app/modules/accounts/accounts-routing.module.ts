a040c6f2517bde51b6da7724f999f90842688b0c
praveen kumar <praveen@textmercato.com>
2021-10-05 10:08:48 +0530

    signup and login page added

--- projects/cpbase-client/src/app/modules/accounts/accounts-routing.module.ts
+++ projects/cpbase-client/src/app/modules/accounts/accounts-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccountShellComponent } from './components/account-shell/account-shell.component';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: AccountShellComponent,
+        children: [
+            {
+                path: 'signup',
+                component: SignupComponent
+            },
+            {
+                path: 'login',
+                component: LoginComponent,
+            },
+            {
+                path: 'forgot-password',
+                component: ForgotPasswordComponent
+            }
+        ]
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- projects/cpbase-client/src/app/modules/accounts/accounts.module.ts
+++ projects/cpbase-client/src/app/modules/accounts/accounts.module.ts
@@ -5,10 +5,11 @@ import { AccountsRoutingModule } from './accounts-routing.module';
 import { SignupComponent } from './pages/signup/signup.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
+import { AccountShellComponent } from './components/account-shell/account-shell.component';
 
 
 @NgModule({
-  declarations: [SignupComponent, LoginComponent, ForgotPasswordComponent],
+  declarations: [SignupComponent, LoginComponent, ForgotPasswordComponent, AccountShellComponent],
   imports: [
     CommonModule,
     AccountsRoutingModule
