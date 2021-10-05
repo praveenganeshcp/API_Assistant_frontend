5a779636cb557e74485443de9771dfb926ddae32
praveen kumar <praveen@textmercato.com>
2021-10-05 14:59:10 +0530

    login form added

--- projects/cpbase-client/src/app/modules/accounts/pages/login/login.component.ts
+++ projects/cpbase-client/src/app/modules/accounts/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,9 +9,28 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-    constructor(private router: Router) { }
+    loginForm: FormGroup;
+
+    constructor(private router: Router, private formBuilder: FormBuilder) { 
+        this.loginForm = this.formBuilder.group({
+            mailId: this.formBuilder.control('', [Validators.required, Validators.email]),
+            password: this.formBuilder.control('', [Validators.required])
+        })
+    }
 
     ngOnInit(): void {
+    }
+
+    loginAccount() {
+        console.log(this.loginForm.value);
+    }
+
+    get mailIdControl() {
+        return this.loginForm.get('mailId');
+    }
+
+    get passwordControl() {
+        return this.loginForm.get('password');
     }
 
     navigate() {
